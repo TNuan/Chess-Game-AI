df70b61cd8180c4750eb71dbd4b3c27725e613d9
Nguyễn Thành Luân <minibike090@gmail.com>
2023-05-13 21:23:10 +0700

    Update UI Options Screen.

---
+++ element.py
@@ -0,0 +1,126 @@
+import pygame
+
+class Button():
+	def __init__(self, image, pos, text_input, font, base_color, hovering_color):
+		self.image = image
+		self.x_pos = pos[0]
+		self.y_pos = pos[1]
+		self.font = font
+		self.base_color, self.hovering_color = base_color, hovering_color
+		self.text_input = text_input
+		self.text = self.font.render(self.text_input, True, self.base_color)
+		if self.image is None:
+			self.image = self.text
+		self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+		self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
+
+	def update(self, screen):
+		if self.image is not None:
+			screen.blit(self.image, self.rect)
+		screen.blit(self.text, self.text_rect)
+
+	def checkForInput(self, position):
+		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+			return True
+		return False
+
+	def changeColor(self, position):
+		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+			self.text = self.font.render(self.text_input, True, self.hovering_color)
+		else:
+			self.text = self.font.render(self.text_input, True, self.base_color)
+
+class OptionBox():
+
+    def __init__(self, x, y, w, h, color, highlight_color, font, option_list, selected = 0):
+        self.color = color
+        self.highlight_color = highlight_color
+        self.rect = pygame.Rect(x, y, w, h)
+        self.font = font
+        self.option_list = option_list
+        self.selected = selected
+        self.draw_menu = False
+        self.menu_active = False
+        self.active_option = -1
+
+    def draw(self, surf):
+        pygame.draw.rect(surf, self.highlight_color if self.menu_active else self.color, self.rect)
+        pygame.draw.rect(surf, (0, 0, 0), self.rect, 2)
+        msg = self.font.render(self.option_list[self.selected], 1, (0, 0, 0))
+        surf.blit(msg, msg.get_rect(center = self.rect.center))
+
+        if self.draw_menu:
+            for i, text in enumerate(self.option_list):
+                rect = self.rect.copy()
+                rect.y += (i+1) * self.rect.height
+                pygame.draw.rect(surf, self.highlight_color if i == self.active_option else self.color, rect)
+                msg = self.font.render(text, 1, (0, 0, 0))
+                surf.blit(msg, msg.get_rect(center = rect.center))
+            outer_rect = (self.rect.x, self.rect.y + self.rect.height, self.rect.width, self.rect.height * len(self.option_list))
+            pygame.draw.rect(surf, (0, 0, 0), outer_rect, 2)
+
+    def update(self, event_list):
+        mpos = pygame.mouse.get_pos()
+        self.menu_active = self.rect.collidepoint(mpos)
+        
+        self.active_option = -1
+        for i in range(len(self.option_list)):
+            rect = self.rect.copy()
+            rect.y += (i+1) * self.rect.height
+            if rect.collidepoint(mpos):
+                self.active_option = i
+                break
+
+        if not self.menu_active and self.active_option == -1:
+            self.draw_menu = False
+
+        for event in event_list:
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                if self.menu_active:
+                    self.draw_menu = not self.draw_menu
+                elif self.draw_menu and self.active_option >= 0:
+                    self.selected = self.active_option
+                    self.draw_menu = False
+                    return self.active_option
+        return -1
+
+class SliderBar:
+    def __init__(self, x, y, width, height, steps, bg_color, bar_color, slider_color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.steps = steps
+        self.bg_color = bg_color
+        self.bar_color = bar_color
+        self.slider_color = slider_color
+
+        self.slider_pos = 0
+        self.step_size = self.width / (self.steps - 1)
+
+    def draw(self, screen):
+        # Draw the background bar
+        bg_rect = pygame.Rect(self.x, self.y + self.height // 2 - 2, self.width, 4)
+        pygame.draw.rect(screen, self.bg_color, bg_rect)
+
+        # Draw the filled part of the bar
+        filled_width = self.slider_pos * self.step_size
+        filled_rect = pygame.Rect(self.x, self.y + self.height // 2 - 2, filled_width, 4)
+        pygame.draw.rect(screen, self.bar_color, filled_rect)
+
+        # Draw the slider
+        slider_radius = self.height // 2
+        slider_center = (self.x + filled_width, self.y + self.height // 2)
+        pygame.draw.circle(screen, self.slider_color, slider_center, slider_radius)
+
+    def update(self, mouse_pos):
+        mouse_x, mouse_y = mouse_pos
+
+        # Check if the mouse is within the bounds of the slider bar
+        if self.x <= mouse_x <= self.x + self.width and self.y <= mouse_y <= self.y + self.height:
+            # Calculate the new slider position based on the mouse position
+            new_slider_pos = round((mouse_x - self.x) / self.step_size)
+            self.slider_pos = max(0, min(new_slider_pos, self.steps - 1))
+
+    def get_value(self):
+        return self.slider_pos
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame, sys
-from button import Button
+from element import Button, OptionBox, SliderBar
 
 pygame.init()
 
@@ -8,7 +8,7 @@ pygame.display.set_caption("Chess game AI")
 
 BG = pygame.image.load("assets/images/background.png")
 
-def get_font(size): # Returns Press-Start-2P in the desired size
+def get_font(size): 
     return pygame.font.Font("assets/font/font.ttf", size)
 
 def play():
@@ -38,22 +38,58 @@ def play():
         pygame.display.update()
     
 def options():
+    
+    mode_options = ["PvP", "PvAI", "AIvAI"]
+    mode_box = OptionBox(160, 220, 300, 50, (193, 167, 132), (100, 100, 100), get_font(30), mode_options)
+
+    AI_options = ["Greedy", "Minimax", "Negamax", "Negascout"]
+    ai_box = OptionBox(800, 220, 300, 50, (193, 167, 132), (100, 100, 100), get_font(30), AI_options)
+
+    slider = SliderBar(520, 490, 400, 30, 4, (193, 167, 132), (100, 100, 100), (117, 98, 73))
+
+    clock = pygame.time.Clock()
+    FPS = 60
     while True:
+        clock.tick(FPS)
         OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
+        SCREEN.blit(pygame.image.load("assets/images/options_bg.png"), (0, 0))
+        SCREEN.blit(pygame.image.load("assets/images/options_blur.png"), (0, 0))
 
-        SCREEN.fill("white")
+        OPTIONS_TEXT = get_font(75).render("SETTING", True, "#55301E")
+        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 100))
 
-        OPTIONS_TEXT = get_font(45).render("This is the OPTIONS screen.", True, "Black")
-        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 260))
+        LEVEL_TEXT = get_font(45).render("Mức độ ", True, "#FFEDD4")
+        LEVEL_RECT = LEVEL_TEXT.get_rect(center=(420, 500))
+
+        MODE_TEXT = get_font(45).render("Chế độ ", True, "#FFEDD4")
+        MODE_RECT = MODE_TEXT.get_rect(center=(320, 180))
+
+        AI_TEXT = get_font(45).render("AI ", True, "#FFEDD4")
+        AI_RECT = AI_TEXT.get_rect(center=(960, 180))
+
         SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
+        SCREEN.blit(LEVEL_TEXT, LEVEL_RECT)
+        SCREEN.blit(MODE_TEXT, MODE_RECT)
+        SCREEN.blit(AI_TEXT, AI_RECT)
 
-        OPTIONS_BACK = Button(image=None, pos=(640, 460), 
-                            text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
+        events = pygame.event.get()
+        # Draw the mode OptionBox and update its state
+        mode_box.draw(SCREEN)
+        ai_box.draw(SCREEN)
+        slider.draw(SCREEN)
+
+        selected_mode = mode_box.update(events)
+        selected_ai = ai_box.update(events)
+        slider.update(OPTIONS_MOUSE_POS)
+        
+
+        OPTIONS_BACK = Button(image=pygame.image.load("assets/images/quit_rect.png"), pos=(640, 660), 
+                            text_input="BACK", font=get_font(50), base_color="#FFEDD4", hovering_color="#C1A784")
 
         OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
         OPTIONS_BACK.update(SCREEN)
 
-        for event in pygame.event.get():
+        for event in events:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
@@ -61,7 +97,11 @@ def options():
                 if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
                     main_menu()
 
-        pygame.display.update()
+        # If a mode has been selected, print it to the console
+        if selected_mode >= 0:
+            print("Selected mode:", mode_options[selected_mode])
+
+        pygame.display.flip()
 
 def main_menu():
     while True:
@@ -71,7 +111,7 @@ def main_menu():
 
         PLAY_BUTTON = Button(image=pygame.image.load("assets/images/play_rect.png"), pos=(640, 250), 
                             text_input="PLAY", font=get_font(75), base_color="#FFEDD4", hovering_color="#C1A784")
-        OPTIONS_BUTTON = Button(image=pygame.image.load("assets/images/options.png"), pos=(640, 400), 
+        OPTIONS_BUTTON = Button(image=pygame.image.load("assets/images/options_rect.png"), pos=(640, 400), 
                             text_input="OPTIONS", font=get_font(75), base_color="#FFEDD4", hovering_color="#C1A784")
         QUIT_BUTTON = Button(image=pygame.image.load("assets/images/quit_rect.png"), pos=(640, 550), 
                             text_input="QUIT", font=get_font(75), base_color="#FFEDD4", hovering_color="#C1A784")
