acb2744487187fd051aa7352ad52a6f5ca87d800
hazel260802 <lehuyentrang260802@gmail.com>
2023-05-24 22:03:03 +0700

    Quiescence Search & Refactoring

---
+++ chess/AI/MiniMax.py
@@ -0,0 +1,61 @@
+from AI.Score import  *
+
+
+def findMoveMinimax(game_state, valid_moves, depth, white_to_move, alpha, beta):
+    global next_move
+    if depth == 0:
+        return quiescenceSearch(game_state, alpha, beta, white_to_move)
+    if white_to_move:
+        max_score = CHECKMATE
+        for move in valid_moves:
+            game_state.makeMove(move)
+            next_moves = game_state.getValidMoves()
+            score = findMoveMinimax(game_state, next_moves, depth - 1, False, alpha, beta)
+            game_state.undoMove()
+            if score > max_score:
+                max_score = score
+                if depth == DEPTH:
+                    next_move = move
+            alpha = max(alpha, max_score)
+            if alpha >= beta:
+                break
+        return max_score
+    else:
+        min_score = CHECKMATE
+        for move in valid_moves:
+            game_state.makeMove(move)
+            next_moves = game_state.getValidMoves()
+            score = findMoveMinimax(game_state, next_moves, depth - 1, True, alpha, beta)
+            game_state.undoMove()
+            if score < min_score:
+                min_score = score
+                if depth == DEPTH:
+                    next_move = move
+            beta = min(beta, min_score)
+            if alpha >= beta:
+                break
+        return min_score
+
+def quiescenceSearch(game_state, alpha, beta, white_to_move):
+    best_score = scoreBoard(game_state)
+    if white_to_move:
+        captures = game_state.getAllPossibleAttacks()
+        for move in captures:
+            game_state.makeMove(move)
+            score = quiescenceSearch(game_state, alpha, beta, False)
+            game_state.undoMove()
+            best_score = max(best_score, score)
+            alpha = max(alpha, best_score)
+            if alpha >= beta:
+                break
+    else:
+        captures = game_state.getAllPossibleAttacks()
+        for move in captures:
+            game_state.makeMove(move)
+            score = quiescenceSearch(game_state, alpha, beta, True)
+            game_state.undoMove()
+            best_score = min(best_score, score)
+            beta = min(beta, best_score)
+            if alpha >= beta:
+                break
+    return best_score
---
+++ chess/AI/Negascout.py
@@ -0,0 +1,45 @@
+from AI.Score import  *
+
+
+def findMoveNegascout(game_state, valid_moves, depth, alpha, beta, turn_multiplier):
+    global next_move
+    best_move = None
+    if depth == 0:
+        return quiescenceSearch(game_state, alpha, beta, turn_multiplier)
+    for i, move in enumerate(valid_moves):
+        game_state.makeMove(move)
+        next_moves = game_state.getValidMoves()
+        if i == 0:
+            score = -findMoveNegascout(game_state, next_moves, depth - 1, -beta, -alpha, -turn_multiplier)
+        else:
+            score = -findMoveNegascout(game_state, next_moves, depth - 1, -alpha - 1, -alpha, -turn_multiplier)  # search with a null window
+            if alpha < score < beta:
+                score = -findMoveNegascout(game_state, next_moves, depth - 1, -beta, -score, -turn_multiplier)  # if it failed high, do a full re-search
+        game_state.undoMove()
+        if alpha < score:
+            best_move = move
+            alpha = score
+        if alpha >= beta:
+            break  # cut-off
+    if depth == DEPTH:
+        next_move = best_move
+    return alpha
+
+def quiescenceSearch(game_state, alpha, beta, turn_multiplier):
+    score = turn_multiplier * scoreBoard(game_state)
+    if score >= beta:
+        return beta
+    if score > alpha:
+        alpha = score
+    capture_moves = game_state.getAllPossibleAttacks()
+    for move in capture_moves:
+        if beta <= alpha:
+            break
+        game_state.makeMove(move)
+        score = -quiescenceSearch(game_state, -beta, -alpha, -turn_multiplier)
+        game_state.undoMove()
+        if score >= beta:
+            return beta
+        if score > alpha:
+            alpha = score
+    return alpha
